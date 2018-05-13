028295d64f8d1f5a17077aacf024523c9377b03b
DestructeurDeChatte <36827612+DestructeurDeChatte@users.noreply.github.com>
2018-05-13 23:02:56 +1000

    Update User.java

--- src/Model/User.java
+++ src/Model/User.java
@@ -4,8 +4,22 @@ import java.util.Scanner;
 
 public abstract class User {
 	
-	private String name;
-	private String id;
-	private String staffType;
+		private String name;
+		private String staffType;
+		private String username;
+		private String password;
+		private String[][] account = {{"jesper", "abc123","casualStaff"},{"christian", "abc123","approver"}};
 
+		public user(String user, String pass, String staff){
+		    username = user;
+		    password = pass;
+		    staffType = staff;
+		}
+
+		public boolean req(){
+			if((username.equals(account[0][0])) && (password.equals(account[0][1])))
+		        return true;
+		    else
+		        return false;
+		}
 }
